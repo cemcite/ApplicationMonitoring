9edadbe8c4ef3a5bf9f75e7adcdd2deb7f2443f3
Cem Çite <cemcite@gmail.com>
2020-06-15 09:25:35 +0300

    SQL scripts

---
+++ ResourceData.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO [dbo].[NSResourcesT] ([Language] ,[ResourceKey] ,[ResourceText])
+     VALUES ('en-US', ,LoginDataError ,'Incorrect username or password')
+GO
+
+INSERT INTO [dbo].[NSResourcesT] ([Language] ,[ResourceKey] ,[ResourceText])
+     VALUES ('en-US', ,PasswordsIncorrect ,'Passwords dont match.')
+GO
+
+INSERT INTO [dbo].[NSResourcesT] ([Language] ,[ResourceKey] ,[ResourceText])
+     VALUES ('en-US', ,UserExist ,'This username is already exists.')
+GO
+
+INSERT INTO [dbo].[NSResourcesT] ([Language] ,[ResourceKey] ,[ResourceText])
+     VALUES ('en-US', ,Success ,'Success')
+GO
+
+INSERT INTO [dbo].[NSResourcesT] ([Language] ,[ResourceKey] ,[ResourceText])
+     VALUES ('en-US', ,Warning ,'Warning')
+GO
+
